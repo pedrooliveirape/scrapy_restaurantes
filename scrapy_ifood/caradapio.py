8150e3c21637a105a18098b9b38ebabe57da0908
pedrooliveirape <pedro.oliveirape@hotmail.com>
2023-01-17 17:31:02 -0300

    cardapio pronto/lojas_pizza criado

--- scrapy_ifood/caradapio.py
+++ scrapy_ifood/caradapio.py
@@ -8,28 +8,92 @@ from time import sleep
 from bs4 import BeautifulSoup
 
 def pegalinksprod(site):
-    lista_html = site.findAll('a', {'data-test-id': 'dish-card-test-id'})#<h3 class="dish-card__description"
-    lista_links = []
+    lista_html = site.findAll('a', {'data-test-id': 'dish-card-test-id'})
+    lista_produtos = []
 
     for i in lista_html:
+        lista_info = []
         link = i.get('href')
-        link = 'https://www.ifood.com.br'+link 
-        lista_links.append(link)
-    return lista_links
+        link = 'https://www.ifood.com.br'+link
+        nomeproduto = i.find('h3', {'class':'dish-card__description'}).text
+        lista_info = [link,nomeproduto]
+        lista_produtos.append(lista_info)
+    return lista_produtos
 
+def abrirproduto(link, nomeproduto):
+    options = Options()
+    options.add_argument('window-size=400,700')
+    options.add_argument('--headless')
+    service = Service(ChromeDriverManager().install())
 
-options = Options()
-options.add_argument('window-size=400,700')
-#options.add_argument('--headless')
-service = Service(ChromeDriverManager().install())
+    enderecoweb = link
+    navegador = webdriver.Chrome(service=service, options=options)
+    navegador.get(enderecoweb)
 
-enderecoweb = 'https://www.ifood.com.br/delivery/igarassu-pe/pizza-em-casa-jardim-boa-sorte/ec6a3997-c0d3-4598-9e0c-4c43b4fee1f6'
-navegador = webdriver.Chrome(service=service, options=options)
-navegador.get(enderecoweb)
+    page_content = navegador.page_source
 
-page_content = navegador.page_source
+    site = BeautifulSoup(page_content, 'html.parser')
 
-site = BeautifulSoup(page_content, 'html.parser')
+    navegador.close()
 
-produtos_links = pegalinksprod(site)
+    nomeproduto = nomeproduto
 
+    if site.find('section', {'class': 'garnish-choices__list'}):
+        list_garnish_choices = site.findAll('section', {'class': 'garnish-choices__list'})
+        lista_itens = []
+        for l in list_garnish_choices:
+            content_garnish_choices = l.findAll('label', {'class': 'garnish-choices__label'})
+            dict_itens = {}
+            lista_itens.append(dict_itens)
+            for i in content_garnish_choices:
+                label_garnish_choices = i
+                try:
+                    nomeitem = label_garnish_choices.p.contents[0]
+                except:
+                    nomeitem = '-'
+                #try:
+                #    detalhesitem = label_garnish_choices.find('span', {'class': 'garnish-choices__option-details'}).text
+                #except:
+                #    detalhesitem = '-'
+                try:
+                    precoitem = label_garnish_choices.find('span', {'class': 'garnish-choices__option-price'}).text
+                except:
+                    precoitem = '-'
+                dict_itens.update({nomeitem: precoitem})
+        precoitensproduto = lista_itens
+    else:
+        #try:
+        #    detalhesproduto = site.find('p', {'class': 'dish-content__container dish-content__details'}).text
+        #except:
+        #    detalhesproduto = '-'
+        try:
+            precoitensproduto = site.find('div', {'class': 'dish-price'}).text
+        except:
+            precoitensproduto = '-'
+    return {nomeproduto: precoitensproduto}
+
+def lojacardapio(enderecoweb):
+    options = Options()
+    options.add_argument('window-size=400,700')
+    options.add_argument('--headless')
+    service = Service(ChromeDriverManager().install())
+
+    endweb = 'https://www.ifood.com.br/delivery/igarassu-pe/pizza-em-casa-jardim-boa-sorte/ec6a3997-c0d3-4598-9e0c-4c43b4fee1f6'
+    navegador = webdriver.Chrome(service=service, options=options)
+    navegador.get(endweb)
+
+    page_content = navegador.page_source
+
+    site = BeautifulSoup(page_content, 'html.parser')
+
+    produtos_links = pegalinksprod(site)
+
+    navegador.close()
+
+    lista_produtos_itens = []
+
+    for i in produtos_links:
+        produto = abrirproduto(i[0],i[1])
+        lista_produtos_itens.append(produto)
+
+    return lista_produtos_itens
--- scrapy_ifood/teste_ifood.py
+++ scrapy_ifood/teste_ifood.py
@@ -7,152 +7,22 @@ from time import sleep
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def sobre(navegador):
-    navegador.find_element(By.XPATH, '//*[@id="__next"]/div[1]/main/div[1]/div/header[2]/div[1]/div/div[2]/button').click()
-    c = 0
-    while c < 10:
-        sleep(1)
-        page_content = navegador.page_source
-        site = BeautifulSoup(page_content, 'html.parser')
-        if site.findAll('p', {'class': 'merchant-details-about__info-data'}):
-            conteudo_endereco = site.findAll('p', {'class': 'merchant-details-about__info-data'})
-
-            endereco = conteudo_endereco[0].text
-            cidade = conteudo_endereco[1].text
-            cep =  conteudo_endereco[2].text[5:]
-            cnpj = conteudo_endereco[3].text[6:]
-            c = 10
-        else:
-            c += 1
-        
-    return [endereco,cidade,cep,cnpj]
-
-def horario(navegador):
-    navegador.find_element(By.ID, 'marmita-tab1-1').click()
-    c = 0
-    while c < 10:
-        sleep(1)
-        page_content = navegador.page_source
-        site = BeautifulSoup(page_content, 'html.parser')
-        if site.findAll('div', {'class': 'merchant-details-schedule__day'}):
-            dias_horarios = site.findAll('div', {'class': 'merchant-details-schedule__day'})
-
-            list_dias_horarios = []
-
-            for dia_horario in dias_horarios:
-                dia = dia_horario.find('span', {'class': 'merchant-details-schedule__day-title-text'}).text
-                horario = dia_horario.find('span', {'class': 'merchant-details-schedule__day-schedule'}).text
-                list_dias_horarios.append(f'{dia} - {horario}')
-            c = 10
-        else:
-            c += 1
-    
-    return list_dias_horarios
-
-def grupos(navegador):
-    c = 0
-    while c < 10:
-        sleep(1)
-        page_content = navegador.page_source
-        site = BeautifulSoup(page_content, 'html.parser')
-
-        if site.findAll('h2', {'class': 'restaurant-menu-group__title'}):
-            grupos_cardapio = site.findAll('h2', {'class': 'restaurant-menu-group__title'})
-            list_grupos = []
-            for grupo in grupos_cardapio:
-                nome_grupo = grupo.text
-                list_grupos.append(nome_grupo.lower())
-            c = 10
-        else:
-            c += 1
-
-    return list_grupos
-
-def scrapypage(nomeloja, enderecoweb):
-    try: # Tentando configurar a página da loja para abertura
-        options = Options()
-        options.add_argument('window-size=400,530')
-        options.add_argument('--headless')
-        service = Service(ChromeDriverManager().install())
-
-        navegador = webdriver.Chrome(service=service, options=options)
-    # Abrindo a página
-        navegador.get(f'{enderecoweb}')
-        
-        try: # Tentando scrapy dos GRUPOS DO CARDÁPIO
-            lista_de_grupos = grupos(navegador)
-        except:
-            lista_de_grupos = ['-']
-
-    # Abrindo o VER MAIS
-        try: # Tentando scrapy do SOBRE
-            lista_sobre = sobre(navegador)
-        except:
-            lista_sobre = ['-','-','-','-']
-        
-        try: #Tentando scrapy do HORÁRIO
-            list_dias = horario(navegador)
-        except:
-            list_dias = ['-']
-
-    # Criando a lista bibliotecas para o Data Frame    
-        navegador.close()
-        return {'NomeLoja': nomeloja,'EnderecoWeb': enderecoweb,'Endereco': lista_sobre[0],'Cidade': lista_sobre[1],'CEP': lista_sobre[2],'CNPJ': lista_sobre[3],'Horario': list_dias,'GrupoCardapio': lista_de_grupos}
-    except:
-        return {'NomeLoja': nomeloja,'EnderecoWeb': enderecoweb,'Endereco': '-','Cidade': '-','CEP': '-','CNPJ': '-','Horario': '-','GrupoCardapio': '-'}
-
-
-df = pd.read_csv('C:\workspace\scrapy_restaurantes\lojas_detalhes_revisar.csv', sep=',')
-dados_comp = pd.read_csv('C:\workspace\scrapy_restaurantes\lojas_detalhes_bruto.csv', sep=';')
-dados_falha = pd.DataFrame(columns=['NomeLoja','EnderecoWeb'])
-lojas_detalhes = []
-lojas_revisar = []
+df = pd.read_csv('C:\Workspace\scrapy_restaurantes\lojas_detalhes_bruto_nao_apagar.csv', sep=';')
 rows = df.shape[0]
-cont = 0
-   
-for i in range(15):
-    nomeloja = df.iloc[i, 0]
-    enderecoweb = df.iloc[i, 1]
-    
-    dadosloja = scrapypage(nomeloja, enderecoweb)
+pizza_df = pd.DataFrame(columns=['NomeLoja','EnderecoWeb'])
+lista_pizza = []
 
-# Inicio teste interação DataFrames --------------
-    if dadosloja['Endereco'] == '-':
-        dadosloja['Endereco'] = df.loc[df.EnderecoWeb==enderecoweb, 'Endereco']
-        dadosloja['Cidade'] = df.loc[df.EnderecoWeb==enderecoweb, 'Cidade']
-        dadosloja['CEP'] = df.loc[df.EnderecoWeb==enderecoweb, 'CEP']
-        dadosloja['CNPJ'] = df.loc[df.EnderecoWeb==enderecoweb, 'CNPJ']
-    if dadosloja['Horario'] == "['-']":
-        dadosloja['Horario'] = df.loc[df.EnderecoWeb==enderecoweb, 'Horario']
-    if dadosloja['GrupoCardapio'] == "['-']":
-        dadosloja['GrupoCardapio'] = df.loc[df.EnderecoWeb==enderecoweb, 'GrupoCardapio']
-# Final teste interação DataFrames --------------
+for i in range(rows):
+    lista_grupos = df.iloc[i, 7]
+    lista_grupos = lista_grupos.replace("'",' ').replace('[','').replace(']','').replace(',','')
+    lista_grupos = lista_grupos.split()
+    #print(type(lista_grupos))
+    for l in lista_grupos:
+        lista_temp = []
+        if 'pizza' in l:
+            lista_temp.append(df.iloc[i, 0])
+            lista_temp.append(df.iloc[i, 1])
+            lista_pizza.append(lista_temp)
 
-    resp = []
-    for k,i in dadosloja.items():
-        a = str(i)
-        resp.append(a)
-    resp = '-' in ''.join(resp)
-    
-    if resp is True:
-        lojas_revisar.append(dadosloja)
-    else:
-        lojas_detalhes.append(dadosloja)
-
-    cont += 1
-    if cont == 5:
-        try:
-            dados_comp = dados_comp.append(lojas_detalhes, ignore_index=False)
-            dados_comp.to_csv('lojas_detalhes_bruto_3.csv', index=False, sep=';')
-            dados_falha = dados_falha.append(lojas_revisar, ignore_index=False)
-            dados_falha.to_csv('lojas_detalhes_revisar_3.csv', index=False, sep=';')
-            lojas_detalhes = []
-            lojas_revisar = []
-            cont = 0
-        except:
-            cont = 0
-# Fim do FOR por loja
-dados_comp = dados_comp.append(lojas_detalhes, ignore_index=False)
-dados_comp.to_csv('lojas_detalhes_bruto_3.csv', index=False, sep=';')
-dados_falha = dados_falha.append(lojas_revisar, ignore_index=False)
-dados_falha.to_csv('lojas_detalhes_revisar_3.csv', index=False, sep=';')
+pizza_df = pizza_df.append(lista_pizza, ignore_index=False)
+pizza_df.to_csv('lojas_pizza.csv', index=False, sep=';')
